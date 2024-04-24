51d12f584bde8794a6a31567e66c105fc5854ad4
fran1709 <fran1709@estudiantec.cr>
2024-04-24 00:53:52 -0600

    put implemented

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -13,24 +13,26 @@ userRoutes.get('/users/:id', async (req, res) => {
     const {id} = req.params;
     const {rows} = await pool.query('SELECT * FROM users WHERE id=$1',[id]);
     if(rows.length === 0){
-        return res.status(404).json({message:'User not found.'})
+        return res.status(404).json({message:'User not found.'});
     }
-    res.json(rows)
+    res.json(rows[0])
 })
 
 // POST
 //
 userRoutes.post('/users', async (req, res) => {
     const data = req.body;
-    const {rows} = await pool.query('INSERT INTO users (name, email) VALUES($1, $2) RETURNING *',[data.name, data.email])
-    return res.json({message:'User created.', rows})
+    const {rows} = await pool.query('INSERT INTO users (name, email) VALUES($1, $2) RETURNING *',[data.name, data.email]);
+    return res.json({message:'User created.', rows});
 })
 
 // PUT
 //
-userRoutes.put('/users/:id', (req, res) => {
+userRoutes.put('/users/:id', async (req, res) => {
     const {id} = req.params;
-    res.send('Actualizando usuario ' + id)
+    const data = req.body;
+    const {rows} = await pool.query('UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *',[data.name, data.email, id]);
+    return res.json({message:'User updated.', rows});
 })
 
 // DELETE
